471b2729699e16043b014568d09059994b1f5fac
yoto88888 <146594953+yoto88888@users.noreply.github.com>
2026-09-06 23:01:42 -0500

    Laboratorio#2-Actividad#1

---
+++ Laboratorio#2 - Actividad#1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibroCalificaciones_1
+{
+    public class LibroCalificacion
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido al libro de calificaciones");
+        }
+    }
+}
---
+++ Laboratorio#2 - Actividad#1/Laboratorio#2 - Actividad#1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LibroCalificaciones_1
+{
+    internal class PruebaLibroCalificaciones_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LibroCalificacion MiLibro = new LibroCalificacion();
+            MiLibro.MostrarMensaje();
+        }
+    }
+}
