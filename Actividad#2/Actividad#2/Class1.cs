5b3d89e00f82b7bf19115020f6d0fc843fac7415
yoto88888 <146594953+yoto88888@users.noreply.github.com>
2026-09-06 23:03:35 -0500

    Laboratorio#2-Actividad#2

---
+++ Actividad#2/Actividad#2/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class MiLibroCalificaciones
+{
+	public void MostarMensaje(string nombreCurso)
+	{
+		Console.WriteLine("Bienvenido al libro de calificaicones para: \n{0} !", nombreCurso);
+	}
+}
---
+++ Actividad#2/Actividad#2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LibroCalificaciones_2
+{
+    public class LibroPruebaCalificaciones
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("hello world!");
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+            //Pide el nombre del curso y lo recibe como entrada
+            Console.WriteLine("Por favor ingrese el nombre del curso: ");
+
+            string nombreDelCurso = Console.ReadLine(); //Lee una linea de texto
+            Console.WriteLine(); //Imprime en pantalla una linea en blanco
+            //llama el metodo MostrarMensaje de MiLibroCalificaciones
+            //y pasa el nombre del curso como argumento
+
+            MyLibro.MostarMensaje(nombreDelCurso);
+        }
+    }
+}
