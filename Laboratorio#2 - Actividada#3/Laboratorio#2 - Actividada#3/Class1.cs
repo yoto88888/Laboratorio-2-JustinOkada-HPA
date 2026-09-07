7fb719e77962233bfba1f458ea53994e1d4e6c7e
yoto88888 <146594953+yoto88888@users.noreply.github.com>
2026-09-06 23:04:48 -0500

    Laboratorio#2-Actividad#3

---
+++ Laboratorio#2 - Actividada#3/Laboratorio#2 - Actividada#3/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class LibroCalificaciones
+{
+	private string nombreCurso;
+
+	public LibroCalificaciones(string nombre)
+	{
+		nombreCurso = nombre;
+	}
+
+	public string NombreCurso
+	{
+		get { return nombreCurso; }
+		set { nombreCurso = value; }
+	}
+
+	public void MostrarMensaje()
+	{
+		Console.WriteLine("Bienvenido al libro de calificaciones para: \n{0}!", NombreCurso);
+	}
+}
---
+++ Laboratorio#2 - Actividada#3/Laboratorio#2 - Actividada#3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Laboratorio_2___Actividada_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear el objeto LibroCalificaciones
+            //Invocar el objeto LibroCalificaciones
+
+            LibroCalificaciones myLibro = new LibroCalificaciones("CS101 Programacion en C#");
+            LibroCalificaciones myLibro2 = new LibroCalificaciones("CS102 Estructura de Datos");
+            Console.Write("El nombre del curso es: {0}\n", myLibro.NombreCurso);
+            Console.Write("El nombre del curso es: {0}\n", myLibro2.NombreCurso);
+
+            //Pide y lee el nombre del curso
+            Console.WriteLine("\nEscriba el nombre del curso: ");
+            string elNombreCurso = Console.ReadLine();
+            myLibro.NombreCurso = elNombreCurso; //Establece el nombre del curso usando una propiedad
+            Console.WriteLine("El nombre del curso es: {0}", myLibro.NombreCurso);
+        }
+    }
+}
